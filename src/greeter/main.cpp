e7c970f7b222761b9c5b617022dc4debbb69b33f
João Távora <joaotavora@gmail.com>
2024-03-19 05:07:20 -0500

    Trucking on with tutorial

--- src/greeter/main.cpp
+++ src/greeter/main.cpp
@@ -1,45 +1,165 @@
 #include <greeter/greeter.h>
 #include <greeter/version.h>
-
-#include <atomic>
-#include <cxxopts.hpp>
 #include <spdlog/spdlog.h>
 
+#include <atomic>
 #include <boost/asio.hpp>
-#include <deque>
-#include <istream>
+#include <cstddef>
+#include <cxxopts.hpp>
+#include <limits>
 #include <memory>
-#include <thread>
+#include <set>
+#include <utility>
+
+#include "boost/asio/buffer.hpp"
+#include "boost/asio/io_context.hpp"
+#include "boost/asio/streambuf.hpp"
 
 namespace asio = boost::asio;
-using tcp = boost::asio::ip::tcp;
+using asio::ip::tcp;
 
+class chat_message;
 
-// ConnectionHandler is the type that is going to be instantiated for
-// each of these connections.
-template <typename ConnectionHandler> class asio_generic_server {
-  using shared_handler_t = std::shared_ptr<ConnectionHandler>;
-
+class chat_participant {
 public:
-  asio_generic_server(int thread_count = 1)
-  : thread_count_(thread_count), acceptor_(ctx_) {}
+  virtual ~chat_participant() {}
+  virtual void deliver(const chat_message& msg) = 0;
+};
 
-  void start_server([[maybe_unused]] uint16_t port) {
-    tcp::endpoint endpoint{tcp::v4(), port};
-    acceptor_.open(endpoint.protocol());
+class chat_message {
+public:
+  chat_message(std::string src, const chat_participant& origin)
+      : what_(std::move(src)), origin_(origin){};
+
+  auto data() { return what_.data(); };
+  size_t size() { return what_.size(); };
+
+  const chat_participant& origin() const { return origin_; }
+
+private:
+  std::string what_;
+  const chat_participant& origin_;
+};
+using chat_message_queue = std::deque<chat_message>;
+
+typedef std::shared_ptr<chat_participant> chat_participant_ptr;
+
+//----------------------------------------------------------------------
+
+class chat_room {
+public:
+  void join(chat_participant* participant) {
+    participants_.insert(participant);
+    for (const auto& msg : recent_msgs_) participant->deliver(msg);
+  }
+
+  void leave(chat_participant* participant) {
+    participants_.erase(participant);
+  }
+
+  void deliver(const chat_message& msg) {
+    recent_msgs_.push_back(msg);
+    while (recent_msgs_.size() > max_recent_msgs) recent_msgs_.pop_front();
+
+    for (auto participant : participants_) {
+      if (&msg.origin() != participant) participant->deliver(msg);
+    }
+  }
+
+private:
+  std::set<chat_participant*> participants_;
+  enum { max_recent_msgs = 100 };
+  chat_message_queue recent_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_session : public chat_participant,
+                     public std::enable_shared_from_this<chat_session> {
+public:
+  chat_session(tcp::socket socket, chat_room& room)
+      : socket_(std::move(socket)), room_(room) {}
+
+  void start() {
+    room_.join(this);
+    read_msg();
+  }
+
+  void deliver(const chat_message& msg) {
+    bool write_in_progress = !write_msgs_.empty();
+    write_msgs_.push_back(msg);
+    if (!write_in_progress) {
+      do_write();
+    }
+  }
+
+private:
+  void read_msg() {
+    asio::async_read_until(
+        socket_, streambuf_, "\r\n",
+        [this, self = shared_from_this()](boost::system::error_code ec,
+                                          std::size_t) {
+          spdlog::info("Yeah got something...!");
+
+          if (ec) {
+            spdlog::warn("Ooops, error '{}', so leaving!", ec.to_string());
+            self->room_.leave(this);
+            return;
+          }
+          std::string str(boost::asio::buffers_begin(streambuf_.data()),
+                          boost::asio::buffers_end(streambuf_.data()));
+          streambuf_.consume(std::numeric_limits<int>::max());
+          spdlog::info("Gonna deliver '{}'", str);
+          self->room_.deliver(chat_message(str, *self));
+          self->read_msg();
+        });
+  }
+
+  void do_write() {
+    asio::async_write(
+        socket_,
+        boost::asio::buffer(write_msgs_.front().data(),
+                            write_msgs_.front().size()),
+        [this, self = shared_from_this()](boost::system::error_code ec,
+                                          std::size_t /*length*/) {
+          if (!ec) {
+            write_msgs_.pop_front();
+            if (!write_msgs_.empty()) {
+              do_write();
+            }
+          } else {
+            room_.leave(this);
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  chat_room& room_;
+  asio::streambuf streambuf_{512};
+  chat_message_queue write_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_server {
+public:
+  chat_server() : acceptor_(ctx_) {}
+
+  void start(uint16_t port) {
+    tcp::endpoint ep{tcp::v4(), port};
+    acceptor_.open(ep.protocol());
     acceptor_.set_option(tcp::acceptor::reuse_address(true));
-    acceptor_.bind(endpoint);
+    acceptor_.bind(ep);
     acceptor_.listen();
     spdlog::info("Should be listening on {}", port);
 
     accept();
-
     for (int i = 0; i < thread_count_; ++i) {
-      thread_pool_.emplace_back([=]{ctx_.run();});
+      thread_pool_.emplace_back([=] { ctx_.run(); });
     }
   }
 
-  void stop_server() {
+  void stop() {
     ctx_.stop();
     for (auto& thread : thread_pool_)
       if (thread.joinable()) thread.join();
@@ -48,86 +168,37 @@ public:
 
 private:
   void accept() {
-    acceptor_.async_accept([=](auto ec, auto connsocket) {
-      handle_new_connection(std::move(connsocket), ec);
-    });
+    spdlog::info("In accept()");
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, tcp::socket socket) {
+          auto ep = socket.remote_endpoint();
+          spdlog::info("Handling new connection from {}:{}",
+                       ep.address().to_string(), ep.port());
+          if (!ec) {
+            spdlog::info("Starting a chat session");
+            std::make_shared<chat_session>(std::move(socket), room_)->start();
+          }
+
+          accept();
+        });
   }
-  template <typename Socket>
-  void handle_new_connection(Socket&& connsocket,
-                             boost::system::error_code const& error) {
-    auto ep = connsocket.remote_endpoint();
-    spdlog::info("Handling new connection from {}:{}",
-                 ep.address().to_string(),
-                 ep.port());
-    auto handler = std::make_shared<ConnectionHandler>(ctx_,
-                                                       std::move(connsocket));
 
-    if (error) {
-      spdlog::info("Returning from handle_new_connection()");
-      return;
-    }
-
-    handler->start();
-
-    accept();
-  }
-  int thread_count_;
+  int thread_count_ = 2;
   std::vector<std::thread> thread_pool_;
   asio::io_context ctx_;
   tcp::acceptor acceptor_;
-};
-
-class chat_handler : public std::enable_shared_from_this<chat_handler> {
-public:
-  chat_handler(asio::io_context& ctx, tcp::socket&& socket)
-  : ctx_(ctx), socket_{std::move(socket)}, write_strand_(ctx) {}
-  tcp::socket& socket() { return socket_; }
-  void start() { read_packet(); }
-
-private:
-  void read_packet(){
-    spdlog::info("Will try to read a packet till EOL");
-    asio::async_read_until(socket_, in_packet_, '\0',
-                           [me = shared_from_this()](auto ec, size_t xfer) {
-                             me->read_packet_done(ec, xfer);
-                           });
-  };
-
-  void read_packet_done(boost::system::error_code ec, [[maybe_unused]]
-                                                      size_t xfer) {
-    if (ec) {
-      spdlog::info("Returning from read_packet_done()");
-      return;
-    }
-
-    std::istream stream(&in_packet_);
-    std::string str;
-    stream >> str;
-
-    spdlog::info("Read a packet '{}'", str);
-
-    read_packet();
-
-  }
-
-  [[maybe_unused]] asio::io_context& ctx_;
-  tcp::socket socket_;
-  asio::io_service::strand write_strand_;
-  asio::streambuf in_packet_;
-  std::deque<std::string> send_packet_queue_;
+  chat_room room_;
 };
 
 std::atomic<bool> server_shutdown = false;
 
 auto main(int argc, char** argv) -> int {
   try {
-
-    
     const std::unordered_map<std::string, greeter::LanguageCode> languages{
-      {"en", greeter::LanguageCode::EN},
-      {"de", greeter::LanguageCode::DE},
-      {"es", greeter::LanguageCode::ES},
-      {"fr", greeter::LanguageCode::FR},
+        {"en", greeter::LanguageCode::EN},
+        {"de", greeter::LanguageCode::DE},
+        {"es", greeter::LanguageCode::ES},
+        {"fr", greeter::LanguageCode::FR},
     };
 
     cxxopts::Options options(*argv, "A program to welcome the world!");
@@ -163,8 +234,8 @@ auto main(int argc, char** argv) -> int {
     }
 
     if (result["server"].as<bool>()) {
-      asio_generic_server<chat_handler> server{};
-      server.start_server(8888);
+      chat_server server{};
+      server.start(8888);
 
       auto signal_handler = [](int signal) {
         spdlog::info("Received {}, going to stop", signal);
@@ -180,7 +251,7 @@ auto main(int argc, char** argv) -> int {
         std::this_thread::sleep_for(1s);
       }
       spdlog::info("Stopping");
-      server.stop_server();
+      server.stop();
       exit(0);
     }
 
