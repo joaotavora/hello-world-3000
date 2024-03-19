2dc98adea7a457df981169623085e112460a950f
João Távora <joaotavora@gmail.com>
2024-03-19 05:50:03 -0500

    Gonna move on to the tests

--- include/greeter/greeter.h
+++ include/greeter/greeter.h
@@ -1,31 +1,135 @@
 #pragma once
 
 #include <string>
+#include <deque>
+#include <set>
+
+#include <greeter/greeter.h>
+#include <greeter/version.h>
+
+#include <boost/asio.hpp>
 
 namespace greeter {
 
-  /**  Language codes to be used with the Greeter class */
-  enum class LanguageCode { EN, DE, ES, FR };
+/**  Language codes to be used with the Greeter class */
+enum class LanguageCode { EN, DE, ES, FR };
+
+/**
+ * @brief A class for saying hello in multiple languages
+ */
+class Greeter {
+  std::string name;
+
+public:
+  /**
+   * @brief Creates a new greeter
+   * @param name the name to greet
+   */
+  Greeter(std::string name);
 
   /**
-   * @brief A class for saying hello in multiple languages
+   * @brief Creates a localized string containing the greeting
+   * @param lang the language to greet in
+   * @return a string containing the greeting
    */
-  class Greeter {
-    std::string name;
+  std::string greet(LanguageCode lang = LanguageCode::EN) const;
+};
 
-  public:
-    /**
-     * @brief Creates a new greeter
-     * @param name the name to greet
-     */
-    Greeter(std::string name);
 
-    /**
-     * @brief Creates a localized string containing the greeting
-     * @param lang the language to greet in
-     * @return a string containing the greeting
-     */
-    std::string greet(LanguageCode lang = LanguageCode::EN) const;
-  };
+namespace asio = boost::asio;
+using asio::ip::tcp;
+
+class chat_message;
+
+class chat_participant {
+public:
+  virtual ~chat_participant() {}
+  virtual void deliver(const chat_message& msg) = 0;
+};
+
+class chat_message {
+public:
+  chat_message(std::string src, const chat_participant& origin)
+  : what_(std::move(src)), origin_(origin){};
+
+  auto data() { return what_.data(); };
+  size_t size() { return what_.size(); };
+
+  const chat_participant& origin() const { return origin_; }
+
+private:
+  std::string what_;
+  const chat_participant& origin_;
+};
+using chat_message_queue = std::deque<chat_message>;
+
+typedef std::shared_ptr<chat_participant> chat_participant_ptr;
+
+//----------------------------------------------------------------------
+
+class chat_room {
+public:
+  void join(chat_participant* participant) {
+    participants_.insert(participant);
+    for (const auto& msg : recent_msgs_) participant->deliver(msg);
+  }
+
+  void leave(chat_participant* participant) {
+    participants_.erase(participant);
+  }
+
+  void deliver(const chat_message& msg);
+
+private:
+  std::set<chat_participant*> participants_;
+  enum { max_recent_msgs = 100 };
+  chat_message_queue recent_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_session : public chat_participant,
+public std::enable_shared_from_this<chat_session> {
+public:
+  chat_session(tcp::socket socket, chat_room& room)
+  : socket_(std::move(socket)), room_(room) {}
+
+  void start() {
+    room_.join(this);
+    read_msg();
+  }
+
+  void deliver(const chat_message& msg);
+
+private:
+  void read_msg();
+
+  void do_write();
+
+  tcp::socket socket_;
+  chat_room& room_;
+  asio::streambuf streambuf_{512};
+  chat_message_queue write_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_server {
+public:
+  chat_server() : acceptor_(ctx_) {}
+
+  void start(uint16_t port);
+
+  void stop();
+
+private:
+  void accept();
+
+  int thread_count_ = 2;
+  std::vector<std::thread> thread_pool_;
+  asio::io_context ctx_;
+  tcp::acceptor acceptor_;
+  chat_room room_;
+};  
 
 }  // namespace greeter
--- src/libgreeter/greeter.cpp
+++ src/libgreeter/greeter.cpp
@@ -2,6 +2,7 @@
 #include <greeter/greeter.h>
 
 #include <boost/asio.hpp>
+#include <spdlog/spdlog.h>
 
 using namespace greeter;
 
@@ -18,5 +19,97 @@ std::string Greeter::greet(LanguageCode lang) const {
       return fmt::format("¡Hola {}!", name);
     case LanguageCode::FR:
       return fmt::format("Bonjour {}!", name);
+  }
+}
+
+void greeter::chat_session::read_msg() {
+  asio::async_read_until(
+      socket_, streambuf_, "\r\n",
+      [this, self = shared_from_this()](boost::system::error_code ec,
+                                        std::size_t) {
+        spdlog::info("Yeah got something...!");
+
+        if (ec) {
+          spdlog::warn("Ooops, error '{}', so leaving!", ec.to_string());
+          self->room_.leave(this);
+          return;
+        }
+        std::string str(boost::asio::buffers_begin(streambuf_.data()),
+                        boost::asio::buffers_end(streambuf_.data()));
+        streambuf_.consume(std::numeric_limits<int>::max());
+        spdlog::info("Gonna deliver '{}'", str);
+        self->room_.deliver(chat_message(str, *self));
+        self->read_msg();
+      });
+}
+
+void greeter::chat_session::do_write() {
+  asio::async_write(socket_,
+                    boost::asio::buffer(write_msgs_.front().data(),
+                                        write_msgs_.front().size()),
+                    [this, self = shared_from_this()](
+                        boost::system::error_code ec, std::size_t /*length*/) {
+                      if (!ec) {
+                        write_msgs_.pop_front();
+                        if (!write_msgs_.empty()) {
+                          do_write();
+                        }
+                      } else {
+                        room_.leave(this);
+                      }
+                    });
+}
+
+void greeter::chat_server::accept() {
+  spdlog::info("In accept()");
+  acceptor_.async_accept(
+      [this](boost::system::error_code ec, tcp::socket socket) {
+        auto ep = socket.remote_endpoint();
+        spdlog::info("Handling new connection from {}:{}",
+                     ep.address().to_string(), ep.port());
+        if (!ec) {
+          spdlog::info("Starting a chat session");
+          std::make_shared<chat_session>(std::move(socket), room_)->start();
+        }
+
+        accept();
+      });
+}
+
+void greeter::chat_server::start(uint16_t port) {
+  tcp::endpoint ep{tcp::v4(), port};
+  acceptor_.open(ep.protocol());
+  acceptor_.set_option(tcp::acceptor::reuse_address(true));
+  acceptor_.bind(ep);
+  acceptor_.listen();
+  spdlog::info("Should be listening on {}", port);
+
+  accept();
+  for (int i = 0; i < thread_count_; ++i) {
+    thread_pool_.emplace_back([this] { ctx_.run(); });
+  }
+}
+
+void greeter::chat_server::stop() {
+  ctx_.stop();
+  for (auto& thread : thread_pool_)
+    if (thread.joinable()) thread.join();
+  thread_pool_.clear();
+}
+
+void greeter::chat_room::deliver(const chat_message& msg) {
+  recent_msgs_.push_back(msg);
+  while (recent_msgs_.size() > max_recent_msgs) recent_msgs_.pop_front();
+
+  for (auto participant : participants_) {
+    if (&msg.origin() != participant) participant->deliver(msg);
+  }
+}
+
+void greeter::chat_session::deliver(const chat_message& msg) {
+  bool write_in_progress = !write_msgs_.empty();
+  write_msgs_.push_back(msg);
+  if (!write_in_progress) {
+    do_write();
   }
 }
