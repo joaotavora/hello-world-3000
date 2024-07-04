71444e8b763af13bb1daec8fd4389647f8063b6b
João Távora <joaotavora@gmail.com>
2024-07-04 21:35:19 +0100

    This is so not going to work

---
+++ conanfile.py
@@ -0,0 +1,16 @@
+from conan import ConanFile
+
+class Recipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+
+    def layout(self):
+        self.folders.generators = "conan"
+
+    def requirements(self):
+        self.requires("fmt/10.2.1")
+        self.requires("boost/1.85.0")
+        self.requires("cxxopts/3.2.0")
+
+    def build_requirements(self):
+        self.test_requires("doctest/2.4.11")
